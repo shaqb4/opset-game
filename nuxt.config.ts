096dec5cb9982ca0ea2a4babade7fdbda2bb1317
Shahked Bleicher <shahkedb4@gmail.com>
2024-11-21 22:36:10 -0500

    Add google analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,10 +7,15 @@ export default defineNuxtConfig({
     '@nuxtjs/tailwindcss',
     '@nuxtjs/color-mode',
     'nuxt-simple-robots',
-    'nuxt-simple-sitemap'
+    'nuxt-simple-sitemap',
+    'nuxt-gtag'
   ],
   site: {
     url: 'https://opset.games',
+  },
+  gtag: {
+    enabled: process.env.NODE_ENV === 'production',
+    id: 'G-8L1FE8DZT7'
   },
   colorMode: {
     preference: 'light', // default theme
